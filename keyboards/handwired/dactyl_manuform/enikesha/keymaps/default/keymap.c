4b5f596521196cef518d2d414dad4c6facf26663
Innocenty Enikeev <enikesha@gmail.com>
2020-01-23 17:09:06 +0300

    [dactyl_enikesha] initial layout

--- keyboards/handwired/dactyl_manuform/enikesha/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/enikesha/keymaps/default/keymap.c
@@ -3,7 +3,6 @@
 //extern keymap_config_t keymap_config;
 
 #define _BASE 0
-#define _FN   1
 
 #define _____ KC_NO
 
@@ -45,44 +44,47 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Base (qwerty)
- * ,----------------------------------------,                        ,----------------------------------------,
- * | TAB  |  q  |   w  |   e  |   r  |   t  |                        |   y  |   u  |   i  |   o  |   p  | \|  |
- * |------+-----+------+------+------+------|                        |------+------+------+------+------+-----|
- * | CAPS |  a  |   s  |   d  |   f  |   g  |                        |   h  |   j  |   k  |   l  |  ;:  | '"  |
- * |------+-----+------+------+------+------|                        |------+------+------+------+------+-----|
- * | CTRL |  z  |   x  |   c  |   v  |   b  |                        |   n  |   m  |   ,  |   .  |  /?  | ALT |
- * '------+-----+------+------+------+------'------+          '------+------+------+------+------+------+-----'
- *              |  {   |   }  | DEL  | BS   | FN   |          | ESC  | SPACE|ENTER |  __  |   +  |
- *              |  [   |   ]  | SHIFT| CMD  | NAV  |          |      | CMD  |SHIFT |   -  |   =  |
- *              '------+------'------+------'------+          '------'------+------'------+------'
+ * ,-----------------------------------,                           ,-----------------------------------,
+ * |  `~ |  q  |  w  |  e  |  r  |  t  |                           |  y  |  u  |  i  |  o  |  p  | [{  |
+ * |-----+-----+-----+-----+-----+-----|                           |-----+-----+-----+-----+-----+-----|
+ * | TAB |  a  |  s  |  d  |  f  |  g  |                           |  h  |  j  |  k  |  l  |  ;: | ]}  |
+ * |-----+-----+-----+-----+-----+-----|                           |-----+-----+-----+-----+-----+-----|
+ * |SHIFT|  z  |  x  |  c  |  v  |  b  |                           |  n  |  m  |  ,< |  .> |  /? | '"  |
+ * '-----------+-----+-----+-----+-----+-----------+   '-----+-----+-----+-----+-----+-----+-----------'
+ *             |MO(1)| ALT |     |BKSPC|CTRL |SUPER|   |ENTER|MO(1)|SPACE|     | ALT |MO(2)|
+ *             '-----+-----'     '-----+-----------+   '-----------+-----'     '-----+-----'
  */
 
-[_BASE] = LAYOUT_mini_4x6( \
-  KC_GRV,  KC_Q,  KC_W,    KC_E,  KC_R,    KC_T,                         KC_Y,    KC_U,  KC_I,    KC_O,   KC_P,   KC_BSLS,\
-  KC_TAB,  KC_A,  KC_S,    KC_D,  KC_F,    KC_G,                         KC_H,    KC_J,  KC_K,    KC_L,   KC_SCLN,KC_QUOT,\
-  KC_LCTL, KC_Z,  KC_X,    KC_C,  KC_V,    KC_B,                         KC_N,    KC_M,  KC_COMM, KC_DOT, KC_SLSH,KC_RALT,\
-                  KC_LBRC, KC_RBRC,                                                      KC_MINS, KC_EQL,                 \
-                                  SFT_DEL,CMD_BSPC, LY_FN,       KC_ESC, CMD_SPC, SFT_ENT                                 \
-),
+LAYOUT_mini_4x6(
+        KC_GRV, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC,
+        KC_TAB, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_RBRC,
+        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_QUOT,
+        MO(1), KC_LALT, KC_RALT, MO(2),
+        KC_BSPC, KC_LCTL, KC_LGUI, KC_ENT, MO(1), KC_SPC),
 
 /* Layer: function + navigation
- * ,----------------------------------------,                        ,----------------------------------------,
- * |      |  F1 |  F2  |  F3  |  F4  |  F5  |                        |  F6  |  F7  |  F8  |  F9  |  F10 | F11 |
- * |------+-----+------+------+------+------|                        |------+------+------+------+------+-----|
- * |      |     |      |      |      |      |                        |      |  <-  |   v  |   -> | mute |     |
- * |------+-----+------+------+------+------|                        |------+------+------+------+------+-----|
- * | CTRL |     |      |      |      |      |                        | home |  end | PgUp | PgDn | vol- |     |
- * '------+-----+------+------+------+------'------+          '------+------+------+------+------+------+-----'
- *              |      |      | DEL  | BS   | FN   |          |      | SPACE|ENTER | <-desktop-> |
- *              |      |      | SHIFT| CMD  | NAV  |          |      | CMD  |SHIFT |             |
- *              '------+------'------+------'------+          '------'------+------'------+------'
+ * ,-----------------------------------,                           ,-----------------------------------,
+ * |  ~  |  1  |  2  |  3  |  4  |  5  |                           |  6  |  7  |  8  |  9  |  0  |  =+ |
+ * |-----+-----+-----+-----+-----+-----|                           |-----+-----+-----+-----+-----+-----|
+ * | ESC |  !  |  @  |  #  |  $  |  %  |                           | home| left| down|  up | left| end |
+ * |-----+-----+-----+-----+-----+-----|                           |-----+-----+-----+-----+-----+-----|
+ * |     |  ^  |  &  |  *  |  (  |  )  |                           |  +  |  -_ | pgdn| pgup|  \  |  |  |
+ * '-----------+-----+-----+-----+-----+-----------+   '-----+-----+-----+-----+-----+-----+-----------'
+ *             | DEL |     |     |     |     |     |   |     |     |     |     |     |     |
+ *             '-----+-----'     '-----+-----------+   '-----------+-----'     '-----+-----'
  */
 
-[_FN] = LAYOUT_mini_4x6( \
-  KC_TAB,  _____,  _____,  _____, _____, _____,                       KC_DEL,  KC_HOME, KC_UP,   KC_PGUP,   KC_VOLU, KC_F12,\
-  _____,   _____,  _____,  _____, _____, _____,                       _____,   KC_LEFT, KC_DOWN, KC_RIGHT,  KC_MUTE, _____, \
-  KC_LCTL, _____,  _____,  _____, _____, _____,                       KC_HOME, KC_END,  KC_PGUP, KC_PGDOWN, KC_VOLD, _____, \
-                   _____,  _____,                                                       CTL_LEFT,  CTL_RGHT,                \
-                                  SFT_DEL,CMD_BSPC, _____,     _____, CMD_SPC, SFT_ENT                                      \
-)
+LAYOUT_mini_4x6(
+        KC_TILD, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_EQL,
+        KC_ESC, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_HOME, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_END,
+        KC_TRNS, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PLUS, KC_MINS, KC_PGDN, KC_PGUP, KC_BSLS, KC_PIPE,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_DEL, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+
+LAYOUT_mini_4x6(
+        RESET, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_F12,
+        KC_TRNS, KC_TRNS, KC_TRNS, LCTL(KC_INS), LSFT(KC_INS), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
 };
